4aac53ebb7e64b707b6a93558c6e89e156bfcbf9
roma <120280212+romaha57@users.noreply.github.com>
2022-12-11 01:01:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ from utils.logging import run_logging
 register_handlers(dp=dp)
 
 if __name__ == '__main__':
-    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=run_logging)
+    run_ligging()
+    executor.start_polling(dispatcher=dp, skip_updates=True)
